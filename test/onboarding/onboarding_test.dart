aa698f92bd29c324ca01db537640a9da5e895437
Robotoaster <swav.kulinski@gmail.com>
2017-06-23 07:09:42 +0100

    onboarding tests stub

---
+++ test/onboarding/onboarding_test.dart
@@ -0,0 +1,28 @@
+import 'package:test/test.dart';
+//import 'package:mockito/mockito.dart';
+
+void main() {
+  test(
+      'GIVEN app starts for the first time WHEN onboarding widget is shown THEN first page is displayed',
+      () {
+    fail('ni!');
+  });
+
+  test(
+      'GIVEN onboarding is shown WHEN user click next button THEN next page is shown',
+      () {
+    fail('ni!');
+  });
+
+  test(
+      'GIVEN onboarding is shown WHEN onboarding shows page 3 THEN next page button shows Continue',
+      () {
+    fail('ni!');
+  });
+
+  test(
+      'GIVEN onboarding is shown WHEN onbording shows first page THEN previous button is disabled',
+      () {
+    fail('ni!');
+  });
+}
