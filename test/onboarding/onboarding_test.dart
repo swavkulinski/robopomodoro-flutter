1866ee1c0cb665c6aa1ac52804f3dd07d4da080e
Robotoaster <swav.kulinski@gmail.com>
2017-06-23 19:05:02 +0100

    attempted di

--- test/onboarding/onboarding_test.dart
+++ test/onboarding/onboarding_test.dart
@@ -1,28 +1,29 @@
-import 'package:test/test.dart';
 //import 'package:mockito/mockito.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../lib/onboarding/onboarding_widget.dart';
 
 void main() {
-  test(
-      'GIVEN app starts for the first time WHEN onboarding widget is shown THEN first page is displayed',
-      () {
-    fail('ni!');
+
+
+      testWidgets('GIVEN app starts for the first time WHEN onboarding widget is shown THEN first page is displayed', (WidgetTester tester) async {
+    // You can use keys to locate the widget you need to test
+    var value = 0.0;
+    var callback = (){};
+
+    // Tells the tester to build a UI based on the widget tree passed to it
+    var widgetUnderTest = new OnboardingWidget(callback: callback);
+    await tester.pumpWidget(widgetUnderTest);
+
+    // Taps on the widget found by key
+    await tester.tap(find.byIcon(Icons.arrow_forward));
+
+    // Verifies that the widget updated the value correctly
+    expect(value, equals(0.5));
   });
 
-  test(
-      'GIVEN onboarding is shown WHEN user click next button THEN next page is shown',
-      () {
-    fail('ni!');
-  });
-
-  test(
-      'GIVEN onboarding is shown WHEN onboarding shows page 3 THEN next page button shows Continue',
-      () {
-    fail('ni!');
-  });
-
-  test(
-      'GIVEN onboarding is shown WHEN onbording shows first page THEN previous button is disabled',
-      () {
-    fail('ni!');
-  });
+      //'GIVEN onboarding is shown WHEN user click next button THEN next page is shown',
+      //'GIVEN onboarding is shown WHEN onboarding shows page 3 THEN next page button shows Continue',
+      //'GIVEN onboarding is shown WHEN onbording shows first page THEN previous button is disabled',
 }
