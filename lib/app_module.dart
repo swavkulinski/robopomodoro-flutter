fa548406b30647dc14ebc1a7e6d87f393d3ade75
Robotoaster <swav.kulinski@gmail.com>
2017-06-27 19:03:59 +0100

    extracted repo and guess what, test are easier to write

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,7 +1,10 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app_state.dart';
+import 'app_repository.dart';
 import 'main.dart';
 
+
 var sharedPreferences = SharedPreferences.getInstance();
-var appStateFactory = () => new AppState(sharedPreferences);
+var appRepository = new AppRepository(sharedPreferences);
+var appStateFactory = () => new AppState(appRepository);
 var robopomodoroApp = new RobopomodoroApp(appStateFactory);
